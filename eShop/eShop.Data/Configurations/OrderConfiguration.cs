2f4cbdc4e1b222208ee929e72136da78da4991c0
Nguyen <gammalux09821@gmail.com>
2020-05-27 18:30:30 +0700

    Entities and configuration

---
+++ eShop/eShop.Data/Configurations/OrderConfiguration.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using eShop.Data.Entities;
+namespace eShop.Data.Configurations
+{
+    public class OrderConfiguration : IEntityTypeConfiguration<Order>
+    {
+        public void Configure(EntityTypeBuilder<Order> builder)
+        {
+            builder.ToTable("Orders");
+            builder.Property(x => x.Id).UseIdentityColumn();
+            builder.Property(x => x.OrderDate).HasDefaultValue(DateTime.Now);
+
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.ShipAddress).IsRequired().HasMaxLength(200);
+            builder.Property(x => x.ShipName).IsRequired().HasMaxLength(200);
+            builder.Property(x => x.ShipPhoneNumber).IsRequired().HasMaxLength(200);
+
+            builder.Property(x => x.ShipEmail).IsRequired().IsUnicode(false).HasMaxLength(50);
+
+        }
+    }
+}
